dca7046a3f0459124cb06edf16d4f5fd48746b29
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-04 23:28:02 +1000

    comparison operators in python

--- 6-comparison-operators/main.py
+++ 6-comparison-operators/main.py
@@ -1,9 +1,8 @@
-a = 1
-b = 2
+condition1 = True
+condition2 = False
 
-print(a == b)
-print(a > b)
-print(a >= b)
-print(a <= b)
-print(a < b)
-print(a != b)
+print(condition1 or condition2)
+print(not condition1 or not condition2)
+print(not condition1)
+print(not condition2)
+print(condition1 and condition2)
