8f950c0c7dd95811d29f70ed3972801b18a5e12d
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-05 00:00:57 +1000

    operators in pythin

--- 7-boolean-operators/main.py
+++ 7-boolean-operators/main.py
@@ -1,9 +1,31 @@
-a = 1
-b = 2
+condition1 = True
+condition2 = False
 
-print(a == b)
-print(a > b)
-print(a >= b)
-print(a <= b)
-print(a < b)
-print(a != b)
+print(condition1 or condition2)
+print(not condition1 or not condition2)
+print(not condition1)
+print(not condition2)
+print(condition1 and condition2)
+
+
+print(0 or 1)
+print (0 and 1)
+print(not 0)
+print(not 1)
+print(not 2)
+print(not 'hey')
+print(not True)
+print(not False)
+print(False or 'hey')
+print('hi' or 'hey')
+print([] or False)
+print([] or True)
+print(False or [])
+print(True or [])
+
+print(False and 'hey')
+print('hi' and 'hey')
+print([] and False)
+print([] and True)
+print(False and [])
+print(True and [])
