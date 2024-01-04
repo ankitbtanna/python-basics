3248992d24a72a31e7af514e49dfc5f0b027c9e8
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-04 13:18:53 +1000

    printing using f statement and string concatenation if statements

--- 2-libraries-lists/main.py
+++ 2-libraries-lists/main.py
@@ -5,18 +5,44 @@ def get_choices():
     options = ['rock', 'paper', 'scissors']
     computer_choice = random.choice(options)
 
+    print('Player chose ' + player_choice)
+    print('Computer chose ' + computer_choice)
+
+    print(f'Player chose: {player_choice} AND Computer chose: {computer_choice}')
+
     choices = {
         'player': player_choice,
         'computer': computer_choice
     }
 
-    return choices
+    winner = check_winner(choices['player'], choices['computer'])
+    return winner
 
 
-choices = get_choices()
-print(choices)
+def check_winner(player, computer):
+    if player == computer:
+        return 'tie'
 
-food = ["pizza", "carrots", "eggs"]
-dinner = random.choice(food)
-print(dinner)
+    if player == 'rock':
+        if computer == 'scissors':
+            return 'player'
+        if computer == 'paper':
+            return 'computer'
 
+    if player == 'paper':
+        if computer == 'rock':
+            return 'player'
+        if computer == 'scissors':
+            return 'computer'
+
+    if player == 'scissors':
+        if computer == 'paper':
+            return 'player'
+        if computer == 'rock':
+            return 'computer'
+
+    return 'Both players must select an option'
+
+
+winner = get_choices()
+print(f'Winner is {winner}')
