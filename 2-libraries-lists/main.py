c6d2d20fb0b02543dc242b96cb5203f68b1e6b7b
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-04 12:57:04 +1000

    import random library use random.choice

---
+++ 2-libraries-lists/main.py
@@ -0,0 +1,22 @@
+import random
+
+def get_choices():
+    player_choice = input("Enter a choice (Rock, Paper, Scissors):")
+    options = ['rock', 'paper', 'scissors']
+    computer_choice = random.choice(options)
+
+    choices = {
+        'player': player_choice,
+        'computer': computer_choice
+    }
+
+    return choices
+
+
+choices = get_choices()
+print(choices)
+
+food = ["pizza", "carrots", "eggs"]
+dinner = random.choice(food)
+print(dinner)
+
