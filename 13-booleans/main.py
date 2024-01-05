76768732aac77aba45a62a4abf159588db85489b
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-05 22:32:16 +1000

    boolean variants in python

---
+++ 13-booleans/main.py
@@ -0,0 +1,25 @@
+done = True
+not_done = False
+
+# Capital T or Capital F
+
+if done:
+    print("yes")
+else:
+    print("no")
+
+if not done:
+    print("no")
+else:
+    print("yes")
+
+
+non_boolean = -1
+
+if non_boolean:
+    print("done")
+else:
+    print("not note")
+
+print(type(done) == bool)
+
