f72b83e5c6437ac76f48299d01a0e7f5f7447349
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-05 22:08:23 +1000

    string methods and string variations

---
+++ 11-string-variations/main.py
@@ -0,0 +1,19 @@
+"Ankit"
+'Ankit'
+name = 'Ankit'
+name += 'Tanna'
+full_name = 'Ankit' + ' ' + 'Tanna'
+print(full_name)
+
+age = 33
+age_string = str(age)
+
+paragraph = """
+This is a lengthy paragraph.
+
+It has multiple lines.
+
+It will print in multiple lines.
+"""
+
+print(paragraph)
---
+++ 12-string-methods/main.py
@@ -0,0 +1,40 @@
+name = 'ankit'
+print(name.upper())
+
+surname = 'TANNA'
+print(surname.lower())
+
+print(name.title())
+
+print(name.islower())
+print(name.istitle())
+print(name.isalpha())
+print(name.isalnum())
+print(name.isascii())
+print(name.isdecimal())
+print(name.startswith('a'))
+print(name.endswith('t'))
+print(name.split(''))
+
+# all return a new string, they do not alter existing string
+
+# isalpha() to check if a string contains only characters and is not empty
+# isalnum() to check if a string contains characters or digits and is not empty
+# isdecimal() to check if a string contains digits and is not empty
+# lower() to get a lowercase version of a string
+# islower() to check if a string is lowercase
+# upper() to get an uppercase version of a string
+# isupper() to check if a string is uppercase
+# isdecimal() to check if a string contains digits and is not empty
+# lower() to get a lowercase version of a string
+# islower() to check if a string is lowercase
+# upper() to get an uppercase version of a string
+# isupper() to check if a string is uppercase
+# title() to get a capitalized version of a string
+# startsswith() to check if the string starts with a specific substring
+# endswith() to check if the string ends with a specific substring
+# replace() to replace a part of a string
+# split() to split a string on a specific character separator
+# strip() to trim the whitespace from a string
+# join() to append new letters to a string
+# find() to find the position of a substring
