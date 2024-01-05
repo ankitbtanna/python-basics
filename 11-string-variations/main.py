6fdc070a56c249b7eb79c7cb4460f634ac15bb8a
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-05 22:20:23 +1000

    escape characters example

--- 11-string-variations/main.py
+++ 11-string-variations/main.py
@@ -8,6 +8,12 @@ print(full_name)
 escape_chars = "An\"kit"
 print(escape_chars)
 
+new_line_char = "Ank\nit"
+print(new_line_char)
+
+escape_escape_chars = "An\\kit"
+print(escape_escape_chars)
+
 age = 33
 age_string = str(age)
 
