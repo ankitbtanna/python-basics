f7312a24cbfa81cb985e50e2957f9bf2641f3ca8
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-06 23:22:58 +1000

    enums in python

---
+++ 16-enums/main.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class State(Enum):
+    INACTIVE = 0
+    ACTIVE = 1
+
+
+print(State.ACTIVE.value)
+print(State.INACTIVE.value)
+
+print(State(0).name)
+print(State(1).name)
+
+print(State['ACTIVE'].value)
+print(State['INACTIVE'].value)
