11b2b2cab7b14badcb8feb8cc79ef457b8e3a527
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-07 00:47:23 +1000

    if elif else control statements

---
+++ 18-control-statements/main.py
@@ -0,0 +1,14 @@
+condition = True
+name = "Roger"
+
+
+if condition:
+    print("Condition is true");
+elif name == "Roger":
+    print("Hello Roger")
+elif name == "Syd":
+    print("Hello Syd")
+elif name == "Flavio":
+    print( "Hello Flavio")
+else:
+    print("Hello")
