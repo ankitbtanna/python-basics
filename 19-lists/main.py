33c398c79bf76a53670006b1d31adb6e3a716b20
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-07 19:55:43 +1000

    lists in python and methods on list

--- 19-lists/main.py
+++ 19-lists/main.py
@@ -25,3 +25,24 @@ print(my_list)
 
 my_list.remove("new item")
 print(my_list)
+
+my_list.insert(2, "new item at 2")
+print(my_list)
+
+# insert multiple items in the list
+my_list[1:1] = ["Test 1", "Test 2", "Test 3"]
+print(my_list)
+
+my_list_nums = [1, 2, 5, 3, 8, 0]
+print(my_list_nums.sort())
+print(my_list_nums)
+
+# sorted() global function does not modify existing array, it returns a new array
+my_list_names = ["Roger", "Beau", "Andy", "Charlie", "beckie", "Karl"]
+sorted_list_names = sorted(my_list_names, key=str.lower)
+print(sorted_list_names)
+print(my_list_names)
+
+copy_my_list = my_list.copy()
+print(copy_my_list)
+
