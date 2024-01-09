80eddabf62fe1d20198bc623f26873e33eead685
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-09 12:07:39 +1000

    external arguments in python while running the program

---
+++ 29-accepting-arguments-from-command-line/dog.py
@@ -0,0 +1,26 @@
+import argparse
+
+def str_to_bool(s):
+    if s.lower() == 'true':
+        return True
+    elif s.lower() == 'false':
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Invalid value for boolean argument")
+
+parser = argparse.ArgumentParser(
+    description="This program takes the input of dog name and breed"
+)
+
+parser.add_argument(
+    '-n', '--name', required=True, type=str, help="The dog's name"
+)
+parser.add_argument(
+    '-b', '--breed', required=True, type=str, help="The dog's breed"
+)
+parser.add_argument(
+    '-v', '--vaccinated', required=True, type=str_to_bool, help="Is the dog vaccinated?"
+)
+
+args = parser.parse_args()
+print(args.name, args.breed, args.vaccinated)
---
+++ 29-accepting-arguments-from-command-line/main.py
@@ -0,0 +1,16 @@
+import sys
+import argparse
+
+print(sys.argv) #  python3 main.py beau 25
+name = sys.argv[1]
+print(name)
+
+print("Hello World!")
+
+parser = argparse.ArgumentParser(
+    description="This program prints the name of my dogs"
+)
+parser.add_argument('-c', '--color', metavar='color', type=str, required=True, help='The color of the dog')
+args = parser.parse_args()
+
+print(args.color)
