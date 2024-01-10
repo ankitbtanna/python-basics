2a966c4669e1daa43a3f437a7ded0b791c21f97c
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-10 23:51:08 +1000

    Annotations in python

---
+++ 35-annotations/main.py
@@ -0,0 +1,17 @@
+# Annotations
+
+def increment(number: int) -> int:
+    """ This function increments the number
+    Args:
+        number (int): Number to be incremented
+
+    Returns:
+        number (int): The incremented number
+    """
+    return number + 1
+
+print(increment(1))
+
+print(help(increment))
+
+increment("abc")
