7f608760fd5cc4fd38d1e937d7c0d318fc8e9a89
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-11 00:48:45 +1000

    list compression syntax

--- 39-list-compressions/main.py
+++ 39-list-compressions/main.py
@@ -1,4 +1,14 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+# simpler
 numbers_power_2 = [num**2 for num in numbers]
 print(numbers_power_2)
+
+# more lines
+num_powers_2_new = []
+for num in numbers:
+    num_powers_2_new.append(num**2)
+print(num_powers_2_new)
+
+numbers_power_2_using_map = list(map(lambda x: x**2, numbers))
+print(numbers_power_2_using_map)
