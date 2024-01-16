d034178d4d95af88bccf8067acad7d123e73db10
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-16 22:53:41 +1000

    how to inherit... Parent-Child class in python

--- 0-object-oriented-programming-python/3-inheritance/main.py
+++ 0-object-oriented-programming-python/3-inheritance/main.py
@@ -57,7 +57,6 @@ class Item:
 
 
 class Phone(Item):
-    all = []
     def __init__(self, name, price, quantity=0, broken_phones=0):
         # Call to super function to have access to all attributes/methods
         super().__init__(
@@ -71,8 +70,6 @@ class Phone(Item):
 
         # Assign of property to self object
         self.broken_phones = broken_phones
-
-        Phone.all.append(self)
 
 
 phone1 = Phone("jscPhonev10", 500, 5, 1)
