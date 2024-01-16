8a599b30fb9a2c44eced6d8c8a74874320bdff7d
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2024-01-16 22:14:56 +1000

    defining class methods and reading from csv

--- 0-object-oriented-programming-python/2-store-in-csv/main.py
+++ 0-object-oriented-programming-python/2-store-in-csv/main.py
@@ -1,5 +1,5 @@
 from typing import List
-import pprint
+import csv
 
 class Item:
     pay_rate = 0.8
@@ -27,19 +27,37 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity'))
+            )
+
     def __repr__(self):
         # return f"Item('{self.name}', {self.price}, {self.quantity})"
         return str(vars(self))
 
 
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)
+
+print("######################")
+Item.instantiate_from_csv()
+print("######################")
 
 
 print(Item.all)
 
 for item in Item.all:
     print(vars(item))
+
+# item1 = Item("Phone", 100, 1)
+# item2 = Item("Laptop", 1000, 3)
+# item3 = Item("Cable", 10, 5)
+# item4 = Item("Mouse", 50, 5)
+# item5 = Item("Keyboard", 75, 5)
